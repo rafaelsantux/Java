c36a91d5e0d3ce08ae5ba5ef5539a593e3bcd927
rafaelsantux <rafaelsantux0411@gmail.com>
2023-05-23 21:44:09 -0300

    Update

---
+++ src/main/java/com/mycompany/cadastro_de_trecos/DbConnection.java
@@ -0,0 +1,16 @@
+package com.mycompany.cadastro_de_trecos;
+
+import java.sql.Connection;
+
+public class DbConnection {
+    
+    private static final String HOSTNAME = "jdbc:mysql://localhost:3306/";
+    private static final String DATABASE = "things";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+    
+    public static Connection dbConnect(){
+        return null;
+    }
+    
+}
